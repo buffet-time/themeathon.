e96a98532e9edd64bc2abcbfa47066efa19ef454
Nic Howe <nicolas.garcia@nyu.edu>
2019-03-12 05:01:56 -0400

    add themeathon social media info

--- src/components/app.component.ts
+++ src/components/app.component.ts
@@ -6,4 +6,15 @@ export default class HelloWorld extends Vue {
 
 	@Prop(String)
 	private msg?: string;
+
+	private items: object[] = [
+		{id: 'discord',
+		 href: 'https://discord.themeathon.com/'},
+		{id: 'twitch',
+		 href: 'https://www.twitch.tv/Themeathon'},
+		{id: 'twitter',
+		 href: 'https://www.twitter.com/Themeathon'},
+		{id: 'youtube',
+		 href: 'http://www.youtube.com/themeathon'}
+	];
 }
