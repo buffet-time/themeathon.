c38f963b75331744bee0193498639679b2a67aec
Nic Howe <nhgarcia27@gmail.com>
2019-03-20 10:52:11 -0400

    Dev/nic (#1)
    * set up basics for markdown
    
    * add more basic info
    
    * add themeathon social media info
    
    * code cleanup
    
    * adjust sizing and position of social media text
    
    * remove unnecessary code
    
    * make the css more readable
    
    * add variables for css colors for readability's sake

--- src/components/app.component.ts
+++ src/components/app.component.ts
@@ -2,8 +2,14 @@ import { Component, Prop, Vue } from 'vue-property-decorator'
 
 @Component
 export default class HelloWorld extends Vue {
-	public blah?: string = 'fasdf'
-
-	@Prop(String)
-	private msg?: string
+	private items: object[] = [
+		{id: 'discord',
+		 href: 'https://discord.themeathon.com/'},
+		{id: 'twitch',
+		 href: 'https://www.twitch.tv/Themeathon'},
+		{id: 'twitter',
+		 href: 'https://www.twitter.com/Themeathon'},
+		{id: 'youtube',
+		 href: 'http://www.youtube.com/themeathon'}
+	];
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,21 @@
 <template>
-  <AppComponent msg="Welcome to Your Vue.js + TypeScript App"/>
+    <AppComponent/>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
-import AppComponent from '@/components/app.component.vue' // @ is an alias to /src
+    import { Component, Vue } from 'vue-property-decorator'
+    import AppComponent from '@/components/app.component.vue' // @ is an alias to /src
 
-@Component({
-	components: {
-		AppComponent
-	}
-})
-export default class Home extends Vue {}
+    @Component({
+        components: {
+            AppComponent
+        }
+    })
+    export default class Home extends Vue {}
 </script>
+
+<style>
+  body {
+        margin: 0;
+  }
+</style>
