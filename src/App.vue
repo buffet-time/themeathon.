e1e9ea78ad201cbaa6f8cd2b6eadf8b27ef09eee
buffet-time <me@danielTurcich.com>
2019-04-08 23:29:37 -0400

    Cleanup of formatting and update of packages

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
-    <img src="@/assets/themeathon.png" alt="" style="width: 128px; height: 128px;"/>
+    <img src="@/assets/themeathon.png" alt style="width: 128px; height: 128px;">
     <div id="nav">
-      <router-link to="/">HOME</router-link> |
+      <router-link to="/">HOME</router-link>|
       <router-link to="/about">ABOUT</router-link>
     </div>
     <router-view/>
@@ -10,30 +10,30 @@
 </template>
 
 <style>
-  @import url('https://fonts.googleapis.com/css?family=Roboto:300,400');
-  #app {
-      font-family: Roboto, Helvetica, Arial, sans-serif;
-      font-weight: 300;
-      -webkit-font-smoothing: antialiased;
-      -moz-osx-font-smoothing: grayscale;
-      text-align: center;
-      color: #fff;
-      background-color: #333;
-  }
-  #nav {
-      padding: 0 30px;
-  }
+@import url('https://fonts.googleapis.com/css?family=Roboto:300,400');
+#app {
+	font-family: Roboto, Helvetica, Arial, sans-serif;
+	font-weight: 300;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #fff;
+	background-color: #333;
+}
+#nav {
+	padding: 0 30px;
+}
 
-  #nav a {
-      font-weight: 300;
-      color: #fc4646;
-  }
+#nav a {
+	font-weight: 300;
+	color: #fc4646;
+}
 
-  #nav a.router-link-exact-active {
-      color: #fc4646;
-  }
+#nav a.router-link-exact-active {
+	color: #fc4646;
+}
 
-  #nav * {
-    text-decoration: none;
-  }
+#nav * {
+	text-decoration: none;
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,21 @@
 <template>
-    <AppComponent/>
+  <AppComponent/>
 </template>
 
 <script lang="ts">
-    import { Component, Vue } from 'vue-property-decorator'
-    import AppComponent from '@/components/app.component.vue' // @ is an alias to /src
+import { Component, Vue } from 'vue-property-decorator'
+import AppComponent from '@/components/app.component.vue' // @ is an alias to /src
 
-    @Component({
-        components: {
-            AppComponent
-        }
-    })
-    export default class Home extends Vue {}
+@Component({
+	components: {
+		AppComponent
+	}
+})
+export default class Home extends Vue {}
 </script>
 
 <style>
-  body {
-        margin: 0;
-  }
+body {
+	margin: 0;
+}
 </style>
