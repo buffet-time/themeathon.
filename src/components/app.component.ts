58490afe8a77b4a254db4986fbe0a67cca2d35ec
Daniel Turcich <me@danielTurcich.com>
2019-04-09 23:56:42 -0400

    Cleanup (#3)
    * Cleanup of formatting and update of packages
    
    * commented out svg that was being referenced that did not exist
    
    * add grid.svg to assets ; uncomment grid html
    
    * once again add grid.svg to assets

--- src/components/app.component.ts
+++ src/components/app.component.ts
@@ -3,13 +3,9 @@ import { Component, Prop, Vue } from 'vue-property-decorator'
 @Component
 export default class HelloWorld extends Vue {
 	private items: object[] = [
-		{id: 'discord',
-		 href: 'https://discord.themeathon.com/'},
-		{id: 'twitch',
-		 href: 'https://www.twitch.tv/Themeathon'},
-		{id: 'twitter',
-		 href: 'https://www.twitter.com/Themeathon'},
-		{id: 'youtube',
-		 href: 'http://www.youtube.com/themeathon'}
-	];
+		{ id: 'discord', href: 'https://discord.themeathon.com/' },
+		{ id: 'twitch', href: 'https://www.twitch.tv/Themeathon' },
+		{ id: 'twitter', href: 'https://www.twitter.com/Themeathon' },
+		{ id: 'youtube', href: 'http://www.youtube.com/themeathon' }
+	]
 }
